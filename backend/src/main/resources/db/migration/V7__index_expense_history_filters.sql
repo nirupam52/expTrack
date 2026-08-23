a546659fb708a19de647ccf1fad606778d8f7e56
Nirupam Bidikar <nirupam52@gmail.com>
2026-08-23 13:32:38 -0500

    fix(db): preserve applied migration history

--- backend/src/main/resources/db/migration/V7__index_expense_history_filters.sql
+++ backend/src/main/resources/db/migration/V7__index_expense_history_filters.sql
@@ -1,23 +1,2 @@
 CREATE INDEX idx_expenses_user_category_date_id
     ON expenses(user_id, category_id, expense_date DESC, id DESC);
-
-CREATE VIRTUAL TABLE expenses_search USING fts5(title, note);
-
-INSERT INTO expenses_search(rowid, title, note)
-    SELECT id, title, coalesce(note, '') FROM expenses;
-
-CREATE TRIGGER expenses_search_after_insert AFTER INSERT ON expenses BEGIN
-    INSERT INTO expenses_search(rowid, title, note)
-        VALUES (new.id, new.title, coalesce(new.note, ''));
-END;
-
-CREATE TRIGGER expenses_search_after_delete AFTER DELETE ON expenses BEGIN
-    DELETE FROM expenses_search WHERE rowid = old.id;
-END;
-
-CREATE TRIGGER expenses_search_after_search_update
-AFTER UPDATE OF title, note ON expenses BEGIN
-    DELETE FROM expenses_search WHERE rowid = old.id;
-    INSERT INTO expenses_search(rowid, title, note)
-        VALUES (new.id, new.title, coalesce(new.note, ''));
-END;
