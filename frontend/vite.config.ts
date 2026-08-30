b293ccf57575def246533f719410d99fecf0902e
Nirupam Bidikar <nirupam52@gmail.com>
2026-08-30 02:00:33 -0500

    build(frontend): remove unused tailwind dependency

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,3 @@
-import tailwindcss from '@tailwindcss/vite';
 import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
@@ -10,7 +9,6 @@ export default defineConfig({
 		}
 	},
 	plugins: [
-		tailwindcss(),
 		sveltekit({
 			compilerOptions: {
 				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
